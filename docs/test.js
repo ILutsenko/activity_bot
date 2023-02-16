684207a0568a7806ae66220bd1dea7350974f464
Луценко Игорь Олегович (4047849) <lucenkoio@vtb.ru>
2023-02-17 00:32:46 +0400

    Добавлена тестовая html страница

--- docs/test.js
+++ docs/test.js
@@ -1,16 +1,17 @@
-// function myFunction(message) {
-//     let user_id = getUserId(message)
-//     console.log(user_id)
-//     let x = new XMLHttpRequest();
-//       x.open("GET", "http://127.0.0.1:80", true);
-//       x.onload = function (){
-//           console.log( x.responseText);
-//       }
-//       x.send(null);
-// }
-function myFunction(message) {
-    alert(message)
-    let user_id = getUserId(message)
+let tg = window.Telegram.WebApp;
+
+tg.expand(); //расширяем на все окно
+
+let task_button = document.getElementById("task_button");
+
+task_button.addEventListener('click', function(){
+	myFunction()
+});
+
+
+
+function myFunction() {
+    let user_id = tg.initDataUnsafe.user.id
     alert(user_id)
     let x = new XMLHttpRequest();
       x.open("GET", "http://127.0.0.1:80", true);
@@ -19,10 +20,3 @@ function myFunction(message) {
       }
       x.send(null);
 }
-
-const getUserId = (message) => {
-  if (message.reply_to_message) {
-    return message.reply_to_message.from.id;
-  }
-  return message.from.id;
-};
