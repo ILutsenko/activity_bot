be2a5974ff0c3fd1342aa0c703257866ecc4df71
Луценко Игорь Олегович (4047849) <lucenkoio@vtb.ru>
2023-02-17 02:26:33 +0400

    Добавлена тестовая html страница

--- docs/test.js
+++ docs/test.js
@@ -2,6 +2,7 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 tg.MainButton.show()
+const url = "http://80.78.248.142:80"
 
 let task_button = document.getElementById("task_button");
 
@@ -12,13 +13,34 @@ task_button.addEventListener('click', function(){
 
 
 
-function myFunction() {
-    let user_id = tg.initDataUnsafe.user.id
-    alert(user_id)
-    let x = new XMLHttpRequest();
-      x.open("GET", "http://127.0.0.1:80", true);
-      x.onload = function (){
-          console.log( x.responseText);
-      }
-      x.send(null);
+// function myFunction() {
+//     let user_id = tg.initDataUnsafe.user.id
+//     let x = new XMLHttpRequest();
+//       x.open("GET", "http://80.78.248.142:80", true);
+//       x.onload = function (){
+//           alert( x.responseText);
+//       }
+//       x.send(null);
+// }
+
+
+const get = async (url, params) => {
+    const response = await fetch(url + '?' + new URLSearchParams(params))
+    const data = await response.json()
+
+    return data
 }
+
+// Call it with async:
+(async () => {
+    const data = await get(url, {
+        user_id: tg.initDataUnsafe.user.id,
+    })
+
+    console.log(data)
+})()
+
+// Calling it with then:
+get(url, {
+    user_id: tg.initDataUnsafe.user.id,
+}).then(data => alert.log(data))
