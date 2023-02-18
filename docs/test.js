6e8398931fa6dcbc71d707794ed8ec09fcbad2b2
Igor <lutsenko99@gmail.com>
2023-02-18 14:53:03 +0400

    remove js dir

--- docs/test.js
+++ docs/test.js
@@ -1,47 +1,41 @@
 let tg = window.Telegram.WebApp;
 const url = "http://80.78.248.142:80"
 let task_button = document.getElementById("task_button");
+let show_button = document.getElementById("show_button");
 
+function make_request(url, method, request_params) {
+    let compiled_url = url + '?' + new URLSearchParams(request_params)
+    let params = {
+        method: method,
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    }
+    return fetch(compiled_url, params).then((response) => {
+        if (response.headers.get("content-type") !== "application/json") {
+            throw new TypeError();
+        }
+        const json_data = response.json();
+        return json_data;
+  });
+}
 
 task_button.addEventListener('click', function(){
-    const form = tg.getElementById('form');
+    const params = {user_id: tg.initDataUnsafe.user.id, task_name: task_name}
+    const form = document.getElementById('form');
     const formData = new FormData(form);
     let task_name = formData.get('text1')
-    post(`${url}/task`, {user_id: tg.initDataUnsafe.user.id, task_name: task_name})
-    alert('event_ended')
+    make_request(`${url}/task`, 'POST', params)
+    alert(`Задача успешно создана: ${task_name}`)
+    createDetailsTag(params.task_name)
+    let container = document.getElementById("task_list_id");
+    let content = container.innerHTML;
+    container.innerHTML= content;
+    document.location.reload()
 });
 
-const post = async (url, params) => {
-    console.log(JSON.stringify(params))
-    let post_url = url + '?' + new URLSearchParams(params)
-    fetch(post_url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        // body: JSON.stringify(params)
-    }).then(response => response).then(
-        response => console.log(response)
-    )
-    createDetailsTag(params.task_name)
-    // tg.reload()
-    var container = document.getElementById("task_list_id");
-    var content = container.innerHTML;
-    container.innerHTML= content;
-    alert('Задача успешно создана')
-}
-
-const get = async (url, params) => {
-    const response = await fetch(url + '?' + new URLSearchParams(params))
-    const data = await response.json()
-    return data
-}
-function get_tasks() {
-    return get(url, {user_id: tg.initDataUnsafe.user.id}).then(
-        data => {return data.message}
-    )
-}
+show_button.addEventListener('click', function(){
+    make_request(url, 'GET', {user_id: tg.initDataUnsafe.user.id}).then(data => console.log(data.message))
+    createDetailsTag()
+});
 
 function addButtons(element){
     const get_task = document.createElement('button');
@@ -49,26 +43,28 @@ function addButtons(element){
     element.appendChild(get_task);
 }
 
-function createDetailsTag(element = NaN){
-    get_tasks(url, {user_id: tg.initDataUnsafe.user.id}).then(tasks_group => {
+function createDetailsTag(){
+    // {user_id: tg.initDataUnsafe.user.id}
+    make_request(url, 'GET', {user_id: tg.initDataUnsafe.user.id}).then(tasks_group => {
         if (!tasks_group){
             alert("У вас нет открытых задач")
         }
-        else if (element){
-            tasks_group = {1: element}
-        }
-        for (const [key, value] of Object.entries(tasks_group)) {
-            console.log(value)
-            const details = document.createElement('details');
-            let par = document.createElement('p')
-            let summary = document.createElement('summary');
-            summary.textContent = value.task_name
-            par.textContent = `Статус задачи: ${value.status}`
-            details.appendChild(summary);
-            details.appendChild(par);
-            addButtons(details)
-            document.getElementById("task_list_id").appendChild(details);
+        let ul = document.getElementById('ul')
+        console.log(tasks_group)
+        for (const [key, value] of Object.entries(tasks_group.message)) {
+            let li = document.createElement('li')
+            li.innerText = value.task_name
+            ul.append(li)
+            // const details = document.createElement('details');
+            // let par = document.createElement('p')
+            // let summary = document.createElement('summary');
+            // summary.textContent = value.task_name
+            // par.textContent = `Статус задачи: ${value.status}`
+            // details.appendChild(summary);
+            // details.appendChild(par);
+            // addButtons(details)
+            // document.getElementById("task_list_id").appendChild(details);
         }
     })
 }
-createDetailsTag()
+// createDetailsTag()
